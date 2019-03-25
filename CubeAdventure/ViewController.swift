5d47eec0c0379e6ea213f7bfc138226f4eb84e68
蕾米莉亚·斯卡雷特 <32812067+feaskters@users.noreply.github.com>
2019-03-25 15:00:22 +0800

    Initial Commit

---
+++ CubeAdventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CubeAdventure
+//
+//  Created by iOS123 on 2019/3/25.
+//  Copyright © 2019年 iOS123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
